40ffc4546bb5017d0acfe38f2c84a05b73b65f54
A.K.M. NAZMUL ISLAM <nazmulislamredoy@gmail.com>
2017-02-25 08:11:46 +0600

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+$(function() {
+    $("#header").load("section/header.html");
+    $("#left-panel-menu").load("section/left-panel.html");
+//    $("#marquee-text").load("section/marquee-text.html");
+    $("#dashboard-right-panel-content").load("section/dashboard-right-panel-content.html");
+    $("#apply-for-leave-content").load("section/apply-for-leave-content.html");
+    $("#leave-details-content").load("section/leave-details-content.html");
+    $("#current-leave-state-content").load("section/current-leave-state-content.html");
+    $("#holiday-calendar-content").load("section/holiday-calendar-content.html");
+    $("#extension-number-content").load("section/extension-number-content.html");
+    $("#own-attendance-content").load("section/own-attendance-content.html");
+    $("#own-current-attendance-content").load("section/own-current-attendance-content.html");
+    $("#absent-summary-content").load("section/absent-summary-content.html");
+    $("#todays-leave-content").load("section/todays-leave-content.html");
+    $("#employee-info-content").load("section/employee-info-content.html");
+    $("#notice-publish-content").load("section/notice-publish-content.html");
+    $("#footer").load("section/footer.html");
+});
+
